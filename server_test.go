68225d36df2ea715ceb5cc6126acd44cd7b92ae9
sdev75 <29279974+sdev75@users.noreply.github.com>
2020-02-21 12:43:15 +0100

    added new test for static files

--- server_test.go
+++ server_test.go
@@ -32,7 +32,7 @@ func TestServer(t *testing.T) {
 	go Start()
 	defer Stop()
 
-	req, err := http.NewRequest("GET", "health", nil)
+	req, err := http.NewRequest("GET", "*", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -47,6 +47,40 @@ func TestServer(t *testing.T) {
 	}
 
 	expected := `{"data":"OK"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got '%v' want '%v'",
+			rr.Body.String(), expected)
+	}
+
+}
+
+func TestServeStaticFile(t *testing.T) {
+
+	router.Match = func(w http.ResponseWriter, r *http.Request) *SimpleRouterMatch {
+		res := &SimpleRouterMatch{}
+		res.Handler = http.FileServer(http.Dir("./public")).ServeHTTP
+		return res
+	}
+
+	Init(router, cfg, nil)
+	go Start()
+	defer Stop()
+
+	req, err := http.NewRequest("GET", "static.txt", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	m := router.Match(rr, req)
+	m.Handler(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got '%v' want '%v'",
+			status, http.StatusOK)
+	}
+
+	expected := `Static file test`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got '%v' want '%v'",
 			rr.Body.String(), expected)
